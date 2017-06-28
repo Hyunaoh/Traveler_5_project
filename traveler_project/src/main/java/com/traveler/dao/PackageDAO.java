b24533697db26ec72468de56006cfdb8ecb781fe
갑부컴\Jiho <kim06300730@hanmail.net>
2017-06-28 11:12:04 +0900

    pull

--- traveler_project/src/main/java/com/traveler/dao/PackageDAO.java
+++ traveler_project/src/main/java/com/traveler/dao/PackageDAO.java
@@ -9,36 +9,22 @@ import org.springframework.stereotype.Repository;
 import com.traveler.model.PackageVO;
 
 @Repository
-public class PackageDAO {
+public interface PackageDAO {
 	
-	@Autowired
-	SqlSession sqlSession;
 	
 	//전체 목록
-	public List<PackageVO> getAllPackage() throws Exception{
-		
-		return sqlSession.getMapper(PackageDAO.class).getAllPackage();	
-		
-	};
+	public List<PackageVO> getAllPackage() throws Exception;
 	
 	//추가
-	public void insertPackage(PackageVO packageVO) throws Exception{
-		
-	};
+	public void insertPackage(PackageVO packageVO) throws Exception;
 	
 	//수정
-	public void updatePackage(PackageVO packageVO) throws Exception{
-		
-	};
+	public void updatePackage(PackageVO packageVO) throws Exception;
 	
 	//삭제
-	public void deletePackage(PackageVO packageVO) throws Exception{
+	public void deletePackage(PackageVO packageVO) throws Exception;
 		
-	};
-	
 	//특정한 목록 찾기
-	public List<PackageVO> getPackage(PackageVO packageVO) throws Exception{
-		return null;
-	};
+	public List<PackageVO> getPackage(PackageVO packageVO) throws Exception;
 
 }
