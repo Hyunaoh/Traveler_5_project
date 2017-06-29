340c006f9522c235a286e179c828c1b617a91bec
김태홍 <7538518@naver.com>
2017-06-29 18:25:18 +0900

    관리자 게시물 관리 기능 추가

--- traveler_project/src/main/java/com/traveler/dao/FindDAO.java
+++ traveler_project/src/main/java/com/traveler/dao/FindDAO.java
@@ -6,23 +6,32 @@ import com.traveler.model.FindVO;
 
 public interface FindDAO {
 
-	// ÀüÃ¼ ¸ñ·Ï °¡Á®¿È
+	// ï¿½ï¿½Ã¼ ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 	public List<FindVO> selectFindListAll() throws Exception;
+	
+	// í˜ì´ì§• ì²˜ë¦¬ë˜ì„œ ì „ì²´ ë¦¬ìŠ¤íŠ¸ ê°€ì ¸ì˜´
+	public List<FindVO> selectFindListAllPaging(FindVO findVO) throws Exception;
 
-	// ¸ñ·Ï Insert
+	// í˜ì´ì§• ì²˜ë¦¬ë˜ì„œ ì „ì²´ ë¦¬ìŠ¤íŠ¸ ê°€ì ¸ì˜´
+	public List<FindVO> selectBadFindListAllPaging(FindVO findVO) throws Exception;
+
+	// ï¿½ï¿½ï¿½ Insert
 	public int insertFindInfo(FindVO findVO) throws Exception;
 
-	// ¸ñ·Ï Update
+	// ï¿½ï¿½ï¿½ Update
 	public int updateFindInfo(FindVO findVO) throws Exception;
 
-	// ¸ñ·Ï Delete
+	// ï¿½ï¿½ï¿½ Delete
 	public int deleteFindInfo(FindVO findVO) throws Exception;
 
-	// Æ¯Á¤ÇÑ Á¤º¸ ÇÏ³ª¸¦ °¡Á®¿È
+	// Æ¯ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½Ï³ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 	public FindVO selectFindInfo(FindVO findVO_in);
 	
 	// ì „ì²´ ê²Œì‹œë¬¼ ê°œìˆ˜ ê°€ì ¸ì˜´
 	public int countFindList() throws Exception;
+
+	// ì „ì²´ ë‚˜ìœ ê²Œì‹œë¬¼ ê°œìˆ˜ ê°€ì ¸ì˜´
+	public int countBadFindList() throws Exception;
 
 	// êµ­ê°€ë³„ ëª©ë¡ ê°€ì ¸ì˜´
 	public List<FindVO> selectCountryFindList(FindVO findVO);
