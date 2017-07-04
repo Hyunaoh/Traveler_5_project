2f03f99bc301582677627d90c24ef955daab1407
mang <puremang@naver.com>
2017-07-04 16:41:44 +0900

    커밋

--- traveler_project/src/main/java/com/traveler/controller/MemberController.java
+++ traveler_project/src/main/java/com/traveler/controller/MemberController.java
@@ -15,6 +15,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.omg.Dynamic.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +42,7 @@ public class MemberController {
 	}
 
 	
+
 	@Transactional
 	@RequestMapping("/memberInsertPro.go")
 	public String memberInsertPro(Model model, MemberVO memberVO) throws Exception {
